9d507cb69d03db4479cee462223b9a45c04a98b1
Abdul Kemp <aakemp786@gmail.com>
2023-06-01 22:31:09 +0200

    update

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-let taskList = []
-let input = document.getElementById('input')
-let list = document.getElementById('list')
+let taskList = [];
+let input = document.getElementById("input");
+let list = document.getElementById("list");
 
-function addTask(){
-    taskList.push(input.value)
-    displayList()
-    // input.addEventListener('click', function(){
-    //     input.style.textDecoration = "Line-through"
-    // })
+function addTask() {
+  taskList.push(input.value);
+  displayList();
+  // input.addEventListener('click', function(){
+  //     input.style.textDecoration = "Line-through"
+  // })
 }
 
-function displayList(){
-    list.innerHTML = ""
-    taskList.forEach((task) => {
-        list.innerHTML += `
-        <input class="check-input" type="checkbox" value="" id="checkbox">` + task 
-    })
-    console.log(list);
+function displayList() {
+  list.innerHTML = "";
+  taskList.forEach((task) => {
+    list.innerHTML +=
+      `<li>
+      <input class="check-input" type="checkbox" value="" id="checkbox">` 
+      + task +
+      `<span><button class="delete" onclick="deleteTask()">x</button></span> 
+      </li>`;
+  });
+  console.log(list);
 }
